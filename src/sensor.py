a23f699c5acce1f209d9d8167dadeea67b18ca4b
j143910 <j143910@tafe.wa.edu.au>
2024-11-21 01:31:20 +0800

    feature: Added constructors and to string methods to Sensor and Display classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,14 @@
 class Sensor:
-    pass
+    def __init__(self, id, car_park, is_active = False):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active
+
+    def __str__(self):
+        return f"""Sensor {self.id} is {'Active' if self.is_active else 'Inactive' }"""
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
