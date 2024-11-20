58bba17737491cc79710cbd1d9f71e48faf11030
j143910 <j143910@tafe.wa.edu.au>
2024-11-21 03:47:47 +0800

    feature: Add core methods to Sensor classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,14 +1,36 @@
-class Sensor:
+from abc import ABC, abstractmethod
+import random
+
+class Sensor(ABC):
     def __init__(self, id, car_park, is_active = False):
         self.id = id
         self.car_park = car_park
         self.is_active = is_active
 
     def __str__(self):
-        return f"""Sensor {self.id} is {'Active' if self.is_active else 'Inactive' }"""
+        return f"Sensor {self.id} is {'Active' if self.is_active else 'Inactive' }"
+
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0,999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
 
 class EntrySensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_entry(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    ...
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
+    def update_car_park(self, plate):
+        self.car_park.remove_entry(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
