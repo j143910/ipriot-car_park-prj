f3d94be46c3a7f1f61b27a915352ee08a1b9ea94
j143910 <j143910@tafe.wa.edu.au>
2024-11-21 08:18:05 +0800

    test: Made Paths class attributes

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -5,8 +5,8 @@ from car_park import CarPark
 
 class TestCarPark(unittest.TestCase):
     def setUp(self):
-        self.car_park = CarPark("123 Example Street", 100)
         self.log_file_path = "new_log.txt"
+        self.car_park = CarPark("123 Example Street", 100, log_file=self.log_file_path)
 
     def test_car_park_initialized_with_all_attributes(self):
         self.assertIsInstance(self.car_park, CarPark)
@@ -16,7 +16,7 @@ class TestCarPark(unittest.TestCase):
         self.assertEqual(self.car_park.sensors, [])
         self.assertEqual(self.car_park.displays, [])
         self.assertEqual(self.car_park.available_bays, 100)
-        self.assertEqual(self.car_park.log_file, Path("log.txt"))
+        self.assertEqual(self.car_park.log_file, Path(self.log_file_path))
 
     def test_add_car(self):
         self.car_park.add_car("FAKE-001")
@@ -55,8 +55,8 @@ class TestCarPark(unittest.TestCase):
 
     def test_car_logged_when_entering(self):
         new_carpark = CarPark("123 Example Street", 100, log_file=self.log_file_path)
-        self.car_park.add_car("NEW-001")
-        with self.car_park.log_file.open() as f:
+        new_carpark.add_car("NEW-001")
+        with new_carpark.log_file.open() as f:
             last_line = f.readlines()[-1]
         self.assertIn("NEW-001", last_line)  # check plate entered
         self.assertIn("entered", last_line)  # check description
@@ -64,13 +64,13 @@ class TestCarPark(unittest.TestCase):
 
     def test_car_logged_when_exiting(self):
         new_carpark = CarPark("123 Example Street", 100, log_file=self.log_file_path)
-        self.car_park.add_car("NEW-001")
-        self.car_park.remove_car("NEW-001")
-        with self.car_park.log_file.open() as f:
+        new_carpark.add_car("NEW-001")
+        new_carpark.remove_car("NEW-001")
+        with new_carpark.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn(last_line, "NEW-001")  # check plate entered
-        self.assertIn(last_line, "exited")  # check description
-        self.assertIn(last_line, "\n")  # check entry has a new line
+        self.assertIn("NEW-001", last_line)  # check plate entered
+        self.assertIn("exited", last_line)  # check description
+        self.assertIn("\n", last_line)  # check entry has a new line
 
     def tearDown(self):
         Path(self.log_file_path).unlink(missing_ok=True)
